5cb4a438a83fbce8d7c290268ef9e7f4ed9719ed
jerome-shiftleft <jerome@theshiftleft.com>
2024-01-13 23:01:52 +0800

    error 404. page not found.

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const http = require("http");
-const express = require('express');
+const express = require("express");
 //const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
 //app.use(bodyParser.urlencoded(extended: false));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 //app.use(express.json());
 app.use(adminRoutes);
 app.use(shopRoutes);
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Error 404. Page not found.</h1>");
+});
 
 // const server = http.createServer(app);
 // server.listen(3000);

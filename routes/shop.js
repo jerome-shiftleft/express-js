270395fbebb52063c85d00c5527c244bc3ca1f5b
jerome-shiftleft <jerome@theshiftleft.com>
2024-01-13 22:58:19 +0800

    progress

---
+++ routes/shop.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  //console.log('In another middleware!');
+  res.send('<h1>Hello from Express!</h1><br /><a href="/add-product">Add Product</a>');
+});
+
+
+module.exports = router;

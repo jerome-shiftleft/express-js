914965b4e712a84d263f249fb0d27e89119098c3
jerome-shiftleft <jerome@theshiftleft.com>
2024-01-13 22:34:20 +0800

    parse test

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 const http = require("http");
-//const routes = require("./routes");
 const express = require('express');
+//const bodyParser = require('body-parser');
 
 const app = express();
 
+//app.use(bodyParser.urlencoded(extended: false));
+app.use(express.urlencoded());
+//app.use(express.json());
+
 app.use('/', (req, res, next) => {
-  console.log('This always runs!');
+  //console.log('This always runs!');
   next();
 });
 
 app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Add Product</h1>');
+  //console.log('In another middleware!');
+  res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/')
 });
 
 app.use('/', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Hello from Express!</h1>');
+  //console.log('In another middleware!');
+  res.send('<h1>Hello from Express!</h1><br /><a href="/add-product">Add Product</a>');
 });
 
 // const server = http.createServer(app);

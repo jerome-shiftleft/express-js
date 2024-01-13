bbeb33328a772132d2daf28ecec567ec66e30b67
jerome-shiftleft <jerome@theshiftleft.com>
2024-01-13 23:14:16 +0800

    filtering paths

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 //app.use(bodyParser.urlencoded(extended: false));
 app.use(express.urlencoded({ extended: true }));
 //app.use(express.json());
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
   res.status(404).send("<h1>Error 404. Page not found.</h1>");
--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   //console.log('In another middleware!');
-  res.send('<h1>Hello from Express!</h1><br /><a href="/add-product">Add Product</a>');
+  res.send('<h1>Hello from Express!</h1><br /><a href="/admin/add-product">Add Product</a>');
 });
 
 

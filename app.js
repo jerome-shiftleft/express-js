ea47680b7b81883d06cdf1ce138cb28c517ff431
jerome-shiftleft <jerome@theshiftleft.com>
2024-01-14 01:45:13 +0800

    progress

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 
 //app.use(bodyParser.urlencoded(extended: false));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.json());
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
